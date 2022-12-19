c9189b42235d00f16f78369ba568d94a11735355
siying <siyinghan@foxmail.com>
2022-12-19 08:26:47 +0100

    v0.0.1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 link_index = 3
 account_list = {"汐琊": 20, "太阳": 16, "卷卷": 9, "温妹舔狗": 9, "画画": 9}
-account_list1 = {"汐琊": 20, "太阳": 16, "温妹舔狗": 9}
+account_list1 = {"汐琊": 20, "太阳": 16, "卷卷": 9}
 
 
 class WeiboAuto:
@@ -142,13 +142,13 @@ class WeiboAuto:
 
 weibo_auto = WeiboAuto()
 
-# for item in account_list1.items():
-#     weibo_auto.send_comments_and_like(item[0], item[1], "表白")
+for item in account_list1.items():
+    weibo_auto.send_comments_and_like(item[0], item[1], "表白")
 
 # weibo_auto.save_cookies("画画")
 
 # account_list = {"汐琊": 27, "太阳": 27, "卷卷": 9, "温妹舔狗": 9, "画画": 9}
 
-for item in account_list.items():
-    if item[0] == "汐琊":
-        weibo_auto.send_comments_and_like(item[0], item[1], "表白")
+# for item in account_list.items():
+#     if item[0] == "汐琊":
+#         weibo_auto.send_comments_and_like(item[0], item[1], "表白")
