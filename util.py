bf3becc73d9ae73d14542f3810f52b219af4b5ee
siying <siyinghan@foxmail.com>
2023-01-18 19:35:17 +0100

    delete unused code

--- util.py
+++ util.py
@@ -8,7 +8,6 @@ import string
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
