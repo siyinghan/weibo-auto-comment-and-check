357ebd28a6c7172a3b04e3718bab2267f772a193
siying <siyinghan@foxmail.com>
2023-02-01 16:46:26 +0100

    update login

--- login_chrome.py
+++ login_chrome.py
@@ -4,6 +4,7 @@ Save login information in the Chrome profile.
 from util import Login
 
 # fill in the account name to log in
-# e.g. Login().run_chrome("xxx")
+account_name = "xxx"
+
 if __name__ == "__main__":
-    Login().run_chrome("")
+    Login().run_chrome(account_name)
