6a48c3c9a971f2eef361fce1c11dc719fd452859
siying <siyinghan@foxmail.com>
2023-02-02 19:03:36 +0100

    update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from multiprocessing import Queue, Process
 from util import CommentSender, CommentChecker
 
 accounts = ["account 1", "account 2", "account 3"]
+link_index = 0
 
 if __name__ == "__main__":
     check_queue = Queue()
-    p1 = Process(target=CommentSender(accounts, 0, check_queue).run, args=())
-    p2 = Process(target=CommentChecker(0, check_queue).run, args=())
+    p1 = Process(target=CommentSender(accounts, link_index, check_queue).run, args=())
+    p2 = Process(target=CommentChecker(link_index, check_queue).run, args=())
     p1.start()
     p2.start()
     p1.join()
