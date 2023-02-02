ed5f0d1bc31ce39876f4c969e0a2afadf8448f89
siying <siyinghan@foxmail.com>
2023-02-02 11:13:45 +0100

    update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ accounts = ["account 1", "account 2", "account 3"]
 
 if __name__ == "__main__":
     check_queue = Queue()
-    p1 = Process(target=CommentSender(accounts, 7, check_queue).run, args=())
-    p2 = Process(target=CommentChecker(7, check_queue).run, args=())
+    p1 = Process(target=CommentSender(accounts, 0, check_queue).run, args=())
+    p2 = Process(target=CommentChecker(0, check_queue).run, args=())
     p1.start()
     p2.start()
     p1.join()
