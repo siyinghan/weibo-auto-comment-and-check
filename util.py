c2a5796dc91968d5965af1581b924a82ad1436e1
siying <siyinghan@foxmail.com>
2023-02-04 15:49:16 +0100

    delete unused code

--- util.py
+++ util.py
@@ -14,7 +14,7 @@ from string import ascii_lowercase
 from time import sleep
 
 from selenium import webdriver
-from selenium.common import NoSuchElementException, ElementClickInterceptedException
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
