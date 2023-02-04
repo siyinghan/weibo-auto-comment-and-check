09d7ba34f28052d47a856914355516c6254f246c
siying <siyinghan@foxmail.com>
2023-02-04 19:31:27 +0100

    update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ Login with different Weibo accounts.
 """
 from multiprocessing import Queue, Process
 
-from util import CommentSender, CommentChecker
+from util import CommentSender, CommentChecker, get_start_account_info
 
 accounts = ["account 1", "account 2", "account 3"]
 link_index = 0
 
 if __name__ == "__main__":
+    get_start_account_info(accounts)
+
     check_queue = Queue()
     p1 = Process(target=CommentSender(accounts, link_index, check_queue).run, args=())
     p2 = Process(target=CommentChecker(link_index, check_queue).run, args=())
